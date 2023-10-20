fd0477e86800d96e0b28c010d24689ce26233162
terramila <67909604+terramila@users.noreply.github.com>
2023-10-20 16:58:28 +0300

    maket of design site

---
+++ maket/comp.js
@@ -0,0 +1,101 @@
+Vue.component("change-Kitchen", {
+  template: `
+              <div class="index2-middle-photo">
+                  <img src="img/Image (7).png" alt="photo" />
+              </div>
+              <div class="index2-middle-time">
+                  <p>26 December,2022</p>
+                  <p>Interior / Home / Decore</p>
+              </div>
+              <p class="index2-middle-text">
+                  A kitchen is a room or part of a room used for cooking and food preparation in a dwelling or in a commercial establishment. A modern middle-class residential kitchen is typically equipped with a stove, a sink with hot and cold running water, a refrigerator, and worktops and kitchen cabinets arranged according to a modular design. Many households have a microwave oven, a dishwasher, and other electric appliances. The main functions of a kitchen are to store, prepare and cook food (and to complete related tasks such as dishwashing). The room or area may also be used for dining (or small meals such as breakfast), entertaining and laundry. The design and construction of kitchens is a huge market all over the world.
+
+                  Commercial kitchens are found in restaurants, cafeterias, hotels, hospitals, educational and workplace facilities, army barracks, and similar establishments. These kitchens are generally larger and equipped with bigger and more heavy-duty equipment than a residential kitchen. For example, a large restaurant may have a huge walk-in refrigerator and a large commercial dishwasher machine. In some instances, commercial kitchen equipment such as commercial sinks is used in household settings as it offers ease of use for food preparation and high durability.
+              </p> `,
+});
+
+Vue.component("change-Bedroom", {
+  template: `
+              <div class="index2-middle-photo">
+                  <img src="img/Simbavati4.jpg" alt="photo" />
+              </div>
+              <div class="index2-middle-time">
+                  <p>26 December,2022</p>
+                  <p>Interior / Home / Decore</p>
+              </div>
+              <p class="index2-middle-text">
+                  Many houses in North America have at least two bedrooms—usually a master bedroom and one or more bedrooms for either children or guests.
+
+                  In some jurisdictions there are basic features (such as a closet and a "means of egress") that a room must have in order to legally qualify as a bedroom. In many states, such as Alaska, bedrooms are not required to have closets and must instead meet minimum size requirements.
+            
+                  A closet by definition is a small space used to store things. In a bedroom, a closet is most commonly used for clothes and other small personal items that one may have. Walk in closets are more popular today and vary in size. However, in the past wardrobes have been the most prominent. A wardrobe is a tall rectangular shaped cabinet that clothes can be stored or hung in. Clothes are also kept in a dresser. Typically nicer clothes are kept in the closet because they can be hung up while leisure clothing and undergarments are stored in the dresser.
+            
+                  In buildings with multiple self-contained housing units (e.g., apartments), the number of bedrooms varies widely. While many such units have at least one bedroom—frequently, these units have at least two—some of these units may not have a specific room dedicated for use as a bedroom. (These units may be known by various names, including studio, efficiency, bedsit, and others.)
+            
+                  Sometimes, a master bedroom is connected to a dedicated bathroom, often called an ensuite or master bathroom.
+              </p>`,
+});
+Vue.component("change-Building", {
+  template: `
+              <div class="index2-middle-photo">
+                  <img src="img/building.jpeg" alt="photo" />
+              </div>
+              <div class="index2-middle-time">
+                  <p>26 December,2022</p>
+                  <p>Interior / Home / Decore</p>
+              </div>
+              <p class="index2-middle-text">
+                  A building or edifice is an enclosed structure with a roof and walls, usually standing permanently in one place, such as a house or factory. Buildings come in a variety of sizes, shapes, and functions, and have been adapted throughout history for numerous factors, from building materials available, to weather conditions, land prices, ground conditions, specific uses, prestige, and aesthetic reasons. To better understand the concept, see Nonbuilding structure for contrast.
+
+                  Buildings serve several societal needs – occupancy, primarily as shelter from weather, security, living space, privacy, to store belongings, and to comfortably live and work. A building as a shelter represents a physical separation of the human habitat (a place of comfort and safety) from the outside (a place that may be harsh and harmful at times).
+
+                  Ever since the first cave paintings, buildings have been objects or canvasses of much artistic expression. In recent years, interest in sustainable planning and building practices has become an intentional part of the design process of many new buildings and other structures, usually green buildings.
+              </p>`,
+});
+Vue.component("change-Architecture", {
+  template: `
+              <div class="index2-middle-photo">
+                  <img src="img/Florence.jpg" alt="photo" />
+              </div>
+              <div class="index2-middle-time">
+                  <p>26 December,2022</p>
+                  <p>Interior / Home / Decore</p>
+              </div>
+              <p class="index2-middle-text">
+                    The practice, which began in the prehistoric era, has been used as a way of expressing culture by civilizations on all seven continents. For this reason, architecture is considered to be a form of art. Texts on architecture have been written since ancient times. The earliest surviving text on architectural theories is the 1st century AD treatise De architectura by the Roman architect Vitruvius, according to whom a good building embodies firmitas, utilitas, and venustas (durability, utility, and beauty). Centuries later, Leon Battista Alberti developed his ideas further, seeing beauty as an objective quality of buildings to be found in their proportions. In the 19th century, Louis Sullivan declared that "form follows function". "Function" began to replace the classical "utility" and was understood to include not only practical but also aesthetic, psychological and cultural dimensions. The idea of sustainable architecture was introduced in the late 20th century.
+
+                    Architecture began as rural, oral vernacular architecture that developed from trial and error to successful replication. Ancient urban architecture was preoccupied with building religious structures and buildings symbolizing the political power of rulers until Greek and Roman architecture shifted focus to civic virtues. Indian and Chinese architecture influenced forms all over Asia and Buddhist architecture in particular took diverse local flavors. During the Middle Ages, pan-European styles of Romanesque and Gothic cathedrals and abbeys emerged while the Renaissance favored Classical forms implemented by architects known by name. Later, the roles of architects and engineers became separated.
+              </p>`,
+});
+Vue.component("change-Kitchen-planning", {
+  template: `
+              <div class="index2-middle-photo">
+                    <img src="img/kitchen.jpeg" alt="photo" />
+                </div>
+                <div class="index2-middle-time">
+                    <p>26 December,2022</p>
+                    <p>Interior / Home / Decore</p>
+                </div>
+                <p class="index2-middle-text">
+                      After starting, read the introductory text and the instructions and you'll be guided safely through our virtual kitchen planner. You have many planning options here to ensure that your dream kitchen becomes a reality: The floor plan can be freely designed, no matter what you hope to achieve, whether you're planning a kitchenette, an L-kitchen, a U-kitchen or a G-kitchen with kitchen counter. The kitchen door and the kitchen windows can be freely placed.
+
+                      You can make your kitchen dream come true with the versatile selection of kitchen furniture: When it comes to kitchen furniture, the base units are available in two cabinet heights, the tall units in four heights and the wall units or wall units in four heights. In addition, there are equipment cabinets, such as tall cabinets for a refrigerator or an oven, sink and hob cabinets; but also stainless steel refrigerators are available as standalone units.
+                </p>`,
+});
+Vue.component("change-Bedroom-planning", {
+  template: `
+            <div class="index2-middle-photo">
+                <img src="img/Master-Bedroom.jpg" alt="photo" />
+            </div>
+            <div class="index2-middle-time">
+                <p>26 December,2022</p>
+                <p>Interior / Home / Decore</p>
+            </div>
+            <p class="index2-middle-text">
+                  Start with the dimensions of your bedroom for your floor plan. After all, no layout makes it possible to put a king-size bed in a tiny 7x10 (6 m2) room and still open the door.
+
+                  The ideal bedroom layout for a smaller bedroom, like a 10x11 (10 m2) room, might have a full- or queen-sized bed centered along the longest wall, flanked by bedside tables lit by sconces.
+
+                  In a small bedroom, you will likely have smaller nightstands, and a sconce hanging above the nightstand will make it seem larger — and give you more room for your books or personal items.
+            </p>`,
+});
